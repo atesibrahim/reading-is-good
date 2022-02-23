647d519fee9c0b843a2cb990f291b3abe9a9d00a
atesibrahim <ates.ibrahimhalil@gmail.com>
2022-02-24 01:39:51 +0300

    added data.sql file for initial load

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,9 @@ INSERT INTO CUSTOMER (Id, Balance) VALUES (3, 8500);
 INSERT INTO BOOK (Id, Price, Stock) VALUES (1, 85, 10);
 INSERT INTO BOOK (id, Price, Stock) VALUES (2, 40, 60);
 INSERT INTO BOOK (id, Price, Stock) VALUES (3, 80, 10);
+
+INSERT INTO ORDERS (Id, Customer_Id, Order_Date, Book_Id, Book_Count, Order_Amount) VALUES (1, 2, null, 1, 10, 1500);
+INSERT INTO ORDERS (id, Customer_Id, Order_Date, Book_Id, Book_Count, Order_Amount) VALUES (2, 2, null, 3, 23, 1500);
+INSERT INTO ORDERS (id, Customer_Id, Order_Date, Book_Id, Book_Count, Order_Amount) VALUES (3, 2, null, 3, 23, 1500);
+INSERT INTO ORDERS (id, Customer_Id, Order_Date, Book_Id, Book_Count, Order_Amount) VALUES (4, 2, null, 2, 23, 1500);
+INSERT INTO ORDERS (id, Customer_Id, Order_Date, Book_Id, Book_Count, Order_Amount) VALUES (5, 1, null, 2, 100, 100);
