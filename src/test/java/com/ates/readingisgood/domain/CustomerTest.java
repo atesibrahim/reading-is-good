c86561204d6da7598ec409817d8c929f23b49630
atesibrahim <ates.ibrahimhalil@gmail.com>
2022-02-24 17:49:23 +0300

    removed unused usages

--- src/test/java/com/ates/readingisgood/domain/CustomerTest.java
+++ src/test/java/com/ates/readingisgood/domain/CustomerTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -31,13 +29,6 @@ public class CustomerTest {
 	public void it_should_balance_equal_by_given_balance() {
 		customer.setBalance(2.500);
 		assertEquals(2.500, customer.getBalance());
-	}
-
-	@Test
-	public void it_should_equal_to_string_by_given()
-	{
-		String expected = "Customer(id=null, balance=null)";
-		assertEquals(expected, customer.toString());
 	}
 
 	@Test
